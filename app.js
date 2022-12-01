72bc34beb06f7ea9f94fed4fe2981d97a66bcc8f
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-01 21:28:32 +0630

    middleware

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middlewares
+app.use(express.json());
+
+// Routes
 app.use('/api/v1/tasks', require('./routes/tasks'));
 
 app.listen(PORT, () => {
