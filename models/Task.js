265d67e39f63c5f59e45ba8babd9fe93e972d835
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-12-03 10:13:06 +0630

    schema validation

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Name cant be empty'],
+    trim: true,
+    maxlength: [20, 'Name cant be more than 20 characters'],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
